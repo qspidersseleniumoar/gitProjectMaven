220985709080f26375d5b8d62c30e1a21cd109c7
qsp-lab3 <qsp-lab3@qsp-lab3-PC>
2018-02-16 23:52:18 +0530

    add

---
+++ gitMAvenProject/src/test/java/com/demo/sampleordertest/OrderTest.java
@@ -0,0 +1,12 @@
+package com.demo.sampleordertest;
+
+import org.testng.annotations.Test;
+
+public class OrderTest {
+	
+	@Test
+	public void createOrderTest() {
+		System.out.println("test");
+	}
+
+}
